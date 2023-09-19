cb010c8ff4274fb85fecf2acef20e64848b134a8
kelvin iriri <139765585+dizzy-101@users.noreply.github.com>
2023-09-19 20:38:26 +0100

    Create akugbenodejs.groovy

---
+++ vars/akugbenodejs.groovy
@@ -0,0 +1,25 @@
+def call(String stageName){
+
+   if ("${stageName}" == "Intro")
+     {
+       sh "echo 'WELCOME TO THE DEPLOYMENT OF AKUGBE NODEJS APP'"
+       sh "echo 'running the CICD akugbe'"
+     }
+  
+  else if ("${stageName}" == "Build")
+     {
+       sh "npm install"
+     }
+  else if ("${stageName}" == "SonarQube Report")
+     { 
+       sh "echo 'running QA on akugbe'"
+       sh "echo 'lets go on akugbe'"
+       //sh "npm run sonar"
+     }
+  else if ("${stageName}" == "Upload Into Nexus")
+     { 
+       sh "echo 'uploading artifacts of akugbe' "
+       sh "echo 'running QA on akugbe'"
+       //sh "npm publish"
+     }
+}
