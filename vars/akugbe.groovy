e42a3f63c3a47dbd6721cc7c73470c15494eb6cd
kelvin iriri <139765585+dizzy-101@users.noreply.github.com>
2023-09-19 16:46:43 +0100

    Update akugbe.groovy

--- vars/akugbe.groovy
+++ vars/akugbe.groovy
@@ -3,7 +3,7 @@ def call(String stageName){
    if ("${stageName}" == "Intro")
      {
        sh "echo 'WELCOME TO THE DEPLOYMENT OF AKUGBE'"
-        sh "echo 'running the CICD akugbe'"
+       sh "echo 'running the CICD akugbe'"
      }
   
   else if ("${stageName}" == "Build")
@@ -13,13 +13,13 @@ def call(String stageName){
   else if ("${stageName}" == "SonarQube Report")
      { 
        sh "echo 'running QA on akugbe'"
-        sh "echo 'lets go on akugbe'"
-        sh "mvn clean sonar:sonar"
+       sh "echo 'lets go on akugbe'"
+       sh "mvn clean sonar:sonar"
      }
   else if ("${stageName}" == "Upload Into Nexus")
      { 
        sh "echo 'uploading artifacts of akugbe' "
-        sh "echo 'running QA on akugbe'"
-        sh "mvn clean deploy"
+       sh "echo 'running QA on akugbe'"
+       sh "mvn clean deploy"
      }
 }
